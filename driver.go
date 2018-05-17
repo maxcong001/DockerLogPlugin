b71e49039719023e9d891747c6b6cb87eab5ae2b
Max <savagecm@users.noreply.github.com>
2018-05-17 13:31:36 +0800

    Update driver.go

--- driver.go
+++ driver.go
@@ -105,7 +105,7 @@ func consumeLog(lf *logPair) {
 		var msg logger.Message
 		msg.Line = buf.Line
 		msg.Source = buf.Source
-		msg.PartialLogMetaData = buf.PartialLogMetaData
+		msg.PLogMetaData = buf.PLogMetaData
 		msg.Timestamp = time.Unix(0, buf.TimeNano)
 		// Max  here need to change
 		if err := lf.l.Log(&msg); err != nil {
@@ -148,7 +148,7 @@ func (d *driver) ReadLogs(info logger.Info, config logger.ReadConfig) (io.ReadCl
 				}
 
 				buf.Line = msg.Line
-				buf.PartialLogMetaData = msg.PartialLogMetaData
+				buf.PLogMetaData = msg.PLogMetaData
 				buf.TimeNano = msg.Timestamp.UnixNano()
 				buf.Source = msg.Source
 
